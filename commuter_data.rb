898d62a4d213c82d419089923c099e0d350f8478
Rob Hill <robert.hill0122@gmail.com>
2014-08-19 11:14:28 -0600

    formatted data

--- commuter_data.rb
+++ commuter_data.rb
@@ -10,7 +10,17 @@ data.each do |commuter|
   array << commuter.to_h
 end
 
-grouped_data =  array.group_by{|commuter| commuter["Person"]}
+grouped_data = array.group_by { |commuter| commuter["Person"] }
+
+grouped_data.each do |commuter|
+  commuter.each do |array|
+    if array.class == Array
+      array.each do |hash|
+        hash.tap {|h| h.delete("Person")}
+      end
+    end
+  end
+end
 
 p grouped_data
 
